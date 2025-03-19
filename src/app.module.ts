de5a74110e3a3e4f1accacd038161777b5686712
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2025-03-18 22:55:20 -0300

    restart again

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsuariosModule } from './usuarios/usuarios.module';
-import { PropiedadesModule } from './propiedades/propiedades.module';
 
 @Module({
-  imports: [AuthModule, UsuariosModule, PropiedadesModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
